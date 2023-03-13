c0015e905e2ea93b80cc8386b2b590ecfe5d4e69
name <your_email>
2023-03-13 09:17:10 +0800

    add progress

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '組員:41023137 林峻源 41023116 吳勁毅', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '組員:41023137 林峻源 41023116 吳勁毅 \n 倉儲: https://github.com/mdecd2023/2a-pj1ag21 \n 網站: https://mdecd2023.github.io/2a-pj1ag21/content/index.html', 'tags': '', 'url': 'About.html'}, {'title': 'progress', 'text': 'w1:講解課程評分 \n w2:228連假放假 \n w3:製作靜態網站 \n w4:BubbleRub tutorial 製作結果與心得', 'tags': '', 'url': 'progress.html'}]};
